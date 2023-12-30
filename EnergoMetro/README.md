1a86160732a800b65fb79a0e30fa908d5236f433
Anagnostakis Ioannis <elahistos@yahoo.gr>
2023-12-30 11:42:04 +0200

    add ads

--- EnergoMetro/README.md
+++
@@ -1,1 +0,0 @@
-[Oροι και Προϋποθέσεις](https://github.com/rizitis/EnergoMetro/blob/main/EnergoMetro_LICENSE.md) χρήσης.
--- EnergoMetro/convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in EnergoMetro_LICENSE.md green.md blue.md yellow.md orange.md; do
-    output="${file%.md}.pdf"
-    pandoc "$file" -o "$output" --pdf-engine=xelatex -V mainfont="DejaVu Serif"
-done
-
